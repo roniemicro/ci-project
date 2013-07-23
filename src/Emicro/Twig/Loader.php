b83cfa040cc4a337a40dd65c08e8d20f9568e0c1
Roni Saha <roni@emicrograph.com>
2013-07-23 13:16:52 +0600

    localize filter added for twig

--- src/Emicro/Twig/Loader.php
+++ src/Emicro/Twig/Loader.php
@@ -1,25 +1,36 @@
 <?php
 /**
  * @Author: Roni Kumar Saha
- * Date: 7/17/13
- * Time: 3:33 PM
+ *        Date: 7/17/13
+ *        Time: 3:33 PM
  */
 
 namespace Emicro\Twig;
 
 
-class Loader extends \Twig_Environment{
-      public function __construct(\Twig_LoaderInterface $loader = null, $options = array())
-      {
-          parent::__construct($loader , $options);
-          $this->initCustomFunctions();
-      }
+class Loader extends \Twig_Environment
+{
+
+    public function __construct(\Twig_LoaderInterface $loader = NULL, $options = array())
+    {
+        parent::__construct($loader, array());
+
+        $this->initCustomFunctions();
+        $this->initCustomFilters();
+    }
+
+    public function translate($value, $domain = FALSE)
+    {
+        return _t($value, $domain);
+    }
+
     private function initCustomFunctions()
     {
-        $function = new \Twig_SimpleFunction('_t', function ($value, $domain=false) {
-            return _t($value, $domain);
-        });
+        $this->addFunction(new \Twig_SimpleFunction('_t', array($this, 'translate')));
+    }
 
-        $this->addFunction($function);
+    private function initCustomFilters()
+    {
+        $this->addFilter(new \Twig_SimpleFilter('localize', array($this, 'translate')));
     }
 }
